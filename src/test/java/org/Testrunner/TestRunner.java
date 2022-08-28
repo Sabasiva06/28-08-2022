26f25d80b5ce177a10b87bf9a41cabf4c8b76e3e
Sabasiva06 <sabasiva06@gmail.com>
2022-08-28 10:35:31 +0530

    TestRunner modification By A

--- src/test/java/org/Testrunner/TestRunner.java
+++ src/test/java/org/Testrunner/TestRunner.java
@@ -6,9 +6,18 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src\\test\\resources\\Features", glue = "org.stepdef", monochrome = true, dryRun=false
+@CucumberOptions(features = "src\\test\\resources\\Features", glue = "org.stepdef", monochrome = true, dryRun = false
 
 )
 public class TestRunner {
+
+	public void report() {
+		System.out.println("Generate Report by JSON");
+
+	}
+
+	public void json() {
+		System.out.println("Generate report by converting Json into html");
+	}
 
 }
