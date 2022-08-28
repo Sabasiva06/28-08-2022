d6892e9265aa7b8c5079c2f81d5a1a0351659edf
Sabasiva06 <sabasiva06@gmail.com>
2022-08-28 10:42:03 +0530

    TestRunner changed by B

--- src/test/java/org/Testrunner/TestRunner.java
+++ src/test/java/org/Testrunner/TestRunner.java
@@ -6,9 +6,18 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src\\test\\resources\\Features", glue = "org.stepdef", monochrome = true, dryRun=false
+@CucumberOptions(features = "src\\test\\resources\\Features", glue = "org.stepdef", monochrome = true, dryRun = false
 
 )
 public class TestRunner {
 
+	public void findigELement() {
+		System.out.println("Find Element by using Locator");
+
+	}
+
+	public void xpath() {
+		System.out.println("Syntax of xpath is : //tagname[@attributename='attribbutevalue']");
+
+	}
 }
